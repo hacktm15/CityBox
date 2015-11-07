ad55d07a2c698ed382363060118cdfc4bfd3cbb4
andrei <bicicleta>
2015-11-07 10:40:25 +0200

    Initial commit

---
+++ www/app/models/Countries.php
@@ -0,0 +1,20 @@
+<?php
+
+class Countries extends \ModelBase
+{
+
+    public $id;
+    public $name;
+    public $abbreviation;
+    public $created;
+    public $modified;
+    public $modified_by;
+
+    public function initialize()
+    {
+
+        // relationships
+        
+    }
+
+}
