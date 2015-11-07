c73462891177233cb49549056f078fb25f9be0af
Florin Horne <thunder86rulz@gmail.com>
2015-11-07 20:30:29 +0200

    area - add

--- www/app/views/area/add.volt
+++ www/app/views/area/add.volt
@@ -21,24 +21,8 @@
 {% endblock %}
 
 {% block jsfooter %}
-    <script type="text/javascript">
-        $(document).ready(function() {
 
-            var map = new ol.Map({
-                target: 'map',
-                layers: [
-                    new ol.layer.Tile({
-                        source: new ol.source.MapQuest({layer: 'sat'})
-                    })
-                ],
-                view: new ol.View({
-                    center: ol.proj.fromLonLat([37.41, 8.82]),
-                    zoom: 4
-                })
-            });
-
-        });
-    </script>
+    {{ javascript_include("/js/app/area/add.js") }}
 
 
 {% endblock %}
--- www/public/js/app/area/add.js
+++ www/public/js/app/area/add.js
@@ -1,15 +1,36 @@
-/**
- * Created by dragos on 07/11/15.
- */
+
+var format = 'image/png';
+
+var tiled = new ol.layer.Tile({
+    visible: false,
+    source: new ol.source.TileWMS({
+        url: 'http://hacktm.lo:8080/geoserver/hacktm/wms',
+        params: {'FORMAT': format,
+            'VERSION': '1.1.1',
+            tiled: true,
+            LAYERS: 'hacktm:utr',
+            STYLES: ''
+        },
+        serverType: 'geoserver'
+    })
+});
+var projection = new ol.proj.Projection({
+    code: 'EPSG:3844',
+    units: 'm',
+    axisOrientation: 'neu'
+});
+
 var map = new ol.Map({
     target: 'map',
+
     layers: [
         new ol.layer.Tile({
             source: new ol.source.MapQuest({layer: 'sat'})
-        })
+        }),
+        tiled
     ],
     view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
+        projection: projection
     })
+
 });
